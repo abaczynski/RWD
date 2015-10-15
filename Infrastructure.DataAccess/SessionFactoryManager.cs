dd785f3b90b5ae89f09ae7cb1ec5050111dd2ff6
abaczynski <baczynski.a@gmail.com>
2015-10-15 14:28:06 +0200

    connection string

--- Infrastructure.DataAccess/SessionFactoryManager.cs
+++ Infrastructure.DataAccess/SessionFactoryManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace Infrastructure.DataAccess
 {
@@ -17,7 +18,7 @@ namespace Infrastructure.DataAccess
 
         public ISessionFactory GetCurrentFactory()
         {
-            string connectionString = @"Server=(local); Database=PurpleBricksDemo; Trusted_Connection=Yes;";
+            string connectionString = ConfigurationManager.ConnectionStrings["PurpleBricks"].ConnectionString;
 
             //string connectionString = @"Server=tcp:idkw0kf851.database.windows.net,1433;Database=rwdcarsAKhefKQPs;User ID=baczynski.a@gmail.com@tmlar4jnxa@idkw0kf851;Password=Lolada123;Trusted_Connection=False;Encrypt=True;Connection Timeout=30";
             ISessionFactory sessionFactory = _dataAccessFactoryBuilder.BuildSessionFactory(connectionString);
